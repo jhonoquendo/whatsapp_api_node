004e7c1f0777b82fb1d62472f822bbec1f60f3f2
jhonoquendo <jhon.oquendo@kushkipagos.com>
2023-11-06 10:59:12 -0500

    Se agregan cambios para webhook y mensajes

--- src/controllers/whatsappController.js
+++ src/controllers/whatsappController.js
@@ -1,9 +1,39 @@
+const fs = require("fs");
+const myConsole = new console.Console(fs.createWriteStream("./logs.txt"));
+
 const verifyToken = (req, res) => {
-    res.send({"data": "verifyToken"})
+
+    try {
+        let accessToken = "askjsadkfsahkfjasksafas";
+        let token = req.query["hub.verify_token"];
+        let challenge = req.query["hub.challenge"];
+
+        if(challenge!=null && token != null && token == accessToken){
+            res.send(challenge);
+        }else{
+            res.status(400).send();
+        }
+
+    } catch (error) {
+        res.status(400).send();
+    }
 }
 
 const receivedMessage = (req, res) => {
-    res.send({"data": "receivedMessage"})
+    try {
+        let entry = req.body["entry"][0];
+        let changes = entry["changes"][0];
+        let value = changes["value"];
+        let messageObject = value["message"];
+
+        myConsole(messageObject);
+
+        res.send("EVENT_RECEIVED");
+
+    } catch (error) {
+        myConsole(error);
+        res.send("EVENT_RECEIVED");
+    }
 }
 
 module.exports = {

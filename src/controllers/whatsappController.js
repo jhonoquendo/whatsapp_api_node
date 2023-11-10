2bdce4b0088a09a237ada6aa8468eda3b42e62f9
jhonoquendo <jhon.oquendo@kushkipagos.com>
2023-11-10 17:11:32 -0500

    Se reciben mensajes

--- src/controllers/whatsappController.js
+++ src/controllers/whatsappController.js
@@ -9,6 +9,7 @@ const verifyToken = (req, res) => {
         let challenge = req.query["hub.challenge"];
 
         if(challenge != null && token != null && token == accessToken){
+            console.log("chanllenge: ", challenge);
             res.send(challenge);
         }else{
             res.status(400).send();
@@ -24,17 +25,45 @@ const receivedMessage = (req, res) => {
         let entry = req.body["entry"][0];
         let changes = entry["changes"][0];
         let value = changes["value"];
-        let messageObject = value["message"];
+        let messageObject = value["messages"];
 
-        //myConsole(messageObject);
+        if (typeof messageObject != "undefined"){
+            let messages = messageObject[0];
+            let text = getTextUser(messages);
+            myConsole.log(text);
+        }
 
-        res.send("EVENT_RECEIVED");
+        res.send("EVENT_RECEIVED OK");
 
     } catch (error) {
         console.log("pasa por aca");
-        //myConsole({error});
-        res.send("EVENT_RECEIVED");
+        myConsole.log({error});
+        res.send("EVENT_RECEIVED MAL");
     }
+}
+
+const getTextUser = (messages) => {
+    let text = "";
+    let typeMessage = messages["type"];
+    if(typeMessage == "text"){
+        text = messages["text"]["body"];
+    }else if (typeMessage == "interactive"){
+        let interactiveObject = messages["interactive"];
+        let typeInteractive = interactiveObject["type"];
+
+        if(typeInteractive == "button_reply"){
+            text = interactiveObject["button_reply"]["title"];
+        }else if(typeInteractive == "list_reply"){
+            text = interactiveObject["list_reply"]["title"];
+        }else{
+            myConsole.log("sin mensaje");
+        }
+
+    }else{
+        myConsole.log("sin mensaje");
+    }
+
+    return text;
 }
 
 module.exports = {

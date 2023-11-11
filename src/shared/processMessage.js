2d2937ba60f0162d2793022cb3adf4f0dbfde516
jhonoquendo <jhon.oquendo@kushkipagos.com>
2023-11-11 11:46:38 -0500

    menu de opciones

--- src/shared/processMessage.js
+++ src/shared/processMessage.js
@@ -9,10 +9,15 @@ const process = (text, number) => {
         //saludar
         let model = whatsappModels.messageText("Hola un gusto saludarte.", number);
         models.push(model);
+
+        let modelList = whatsappModels.messageList(number);
+        models.push(modelList);
+
     }else if(text.includes("gracias")){
         //agradecer
         let model = whatsappModels.messageText("Gracias a ti por escribirme", number);
         models.push(model);
+
     }else if(text.includes("adios") || 
              text.includes("adiós") ||
              text.includes("bye") ||
@@ -20,6 +25,16 @@ const process = (text, number) => {
                 //despedir
         let model = whatsappModels.messageText("Ve con cuidado.", number);
         models.push(model);
+    }else if(text.includes("comprar")){
+        //comprar
+        let model = whatsappModels.messageComprar(number);
+        models.push(model);
+
+    }else if(text.includes("vender")){
+        //vender
+        let model = whatsappModels.messageText("Registrate en este formulario: www.google.com", number);
+        models.push(model);
+
     }else{
         //no entiende
         let model = whatsappModels.messageText("No entiendo.", number);
--- src/shared/whatsappModels.js
+++ src/shared/whatsappModels.js
@@ -5,6 +5,7 @@ const messageText = (text, number) => {
         "recipient_type": "individual",
         "type":"text",
         "text":{
+            "preview_url": true,
             "body": text
         }
     });
@@ -12,6 +13,98 @@ const messageText = (text, number) => {
     return data;
 }
 
+const messageList = (number) => {
+    const data = JSON.stringify({
+        "messaging_product": "whatsapp",
+        "recipient_type": "individual",
+        "to": number,
+        "type": "interactive",
+        "interactive": {
+            "type": "list",
+            "body": {
+                "text": "Tengo estas opciones"
+            },
+            "footer": {
+                "text": "Selecciona una de las opciones para poder atenderte"
+            },  
+            "action": {
+                "button": "Ver opciones",
+                "sections": [
+                    {
+                        "title": "Compra y vende productos",
+                        "rows": [
+                            {
+                                "id": "main-comprar",
+                                "title": "Comprar",
+                                "description": "Compra los mejores productos para tu hogar"
+                            },
+                            {
+                                "id": "main-vender",
+                                "title": "Vender",
+                                "description": "Vende los mejores productos para tu hogar"
+                            }
+                        ]
+                    },
+                    {
+                        "title": "Centro de atencion",
+                        "rows": [
+                            {
+                                "id": "main-agencia",
+                                "title": "Agencia",
+                                "description": "Agencia los mejores productos para tu hogar"
+                            },
+                            {
+                                "id": "main-contacto",
+                                "title": "Contacto",
+                                "description": "Contacta los mejores productos para tu hogar"
+                            }
+                        ]
+                    }
+                ]
+            }
+        }
+    });
+
+    return data;
+}
+
+const messageComprar= (number) => {
+    const data = JSON.stringify({
+        "messaging_product": "whatsapp",
+        "recipient_type": "individual",
+        "to": number,
+        "type": "interactive",
+        "interactive": {
+            "type": "button",
+            "body": {
+                "text": "Selecciona uno de los productos"
+            },
+            "action": {
+                "buttons": [
+                    {
+                        "type": "reply",
+                        "reply": {
+                            "id": "option-laptop",
+                            "title": "Laptop"
+                        }
+                    },
+                    {
+                        "type": "reply",
+                        "reply": {
+                            "id": "option-computadora",
+                            "title": "Computadora"
+                        }
+                    }
+                ]
+            }
+        }
+    });
+
+    return data;
+}
+
 module.exports = {
-    messageText
+    messageText,
+    messageList,
+    messageComprar
 }
